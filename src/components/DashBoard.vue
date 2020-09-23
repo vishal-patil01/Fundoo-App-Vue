aa4ddc2b89a8e69c02f044e31474ced6cc4fa7a5
vishal-patil01 <vishalpatil01@hotmail.com>
2020-09-23 21:22:49 +0530

    Add : Dashboard Component with app bar and drawer.

---
+++ src/components/DashBoard.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="page-container md-layout-column">
+    <md-toolbar class="app-bar">
+      <md-button class="md-icon-button" @click="showNavigation = true">
+        <md-icon>menu</md-icon>
+      </md-button>
+      <span class="md-title">Fundoo App</span>
+    </md-toolbar>
+
+    <md-drawer :md-active.sync="showNavigation" md-swipeable>
+      <md-toolbar class="md-transparent" md-elevation="0">
+        <span class="md-title">Fundoo App</span>
+      </md-toolbar>
+
+      <md-list>
+        <md-list-item>
+          <md-icon>move_to_inbox</md-icon>
+          <span class="md-list-item-text">Inbox</span>
+        </md-list-item>
+
+        <md-list-item>
+          <md-icon>send</md-icon>
+          <span class="md-list-item-text">Sent Mail</span>
+        </md-list-item>
+
+        <md-list-item>
+          <md-icon>delete</md-icon>
+          <span class="md-list-item-text">Trash</span>
+        </md-list-item>
+
+        <md-list-item>
+          <md-icon>error</md-icon>
+          <span class="md-list-item-text">Spam</span>
+        </md-list-item>
+      </md-list>
+    </md-drawer>
+    <md-content>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam.</md-content>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "DashBoard",
+  data: () => ({
+    showNavigation: false,
+    showSidepanel: false,
+  }),
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.page-container {
+  min-height: 300px;
+  overflow: hidden;
+  position: relative;
+  border: 1px solid rgba(#000, 0.12);
+  background-color: white;
+}
+
+// Demo purposes only
+.md-drawer {
+  width: 230px;
+  max-width: calc(100vw - 125px);
+}
+
+.md-content {
+  padding: 16px;
+}
+.app-bar {
+  background-color: rgb(237, 180, 21);
+  color: white;
+  z-index: 5;
+}
+</style>
