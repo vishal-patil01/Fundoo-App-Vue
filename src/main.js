b302c508e5a6c1853c5f079ed17d33ccd7e86b49
vishal-patil01 <vishalpatil01@hotmail.com>
2020-09-23 19:01:09 +0530

    Initial Commit : Fundoo App Project Created.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Style from './styles/style.scss'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 
 Vue.config.productionTip = false
+Vue.use(VueMaterial)
+Vue.use(Style)
 
 new Vue({
   render: h => h(App),
+  router: router
 }).$mount('#app')
