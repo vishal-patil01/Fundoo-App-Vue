98fe9f5433b422e64f2e54bc4ce46ac400e16cde
vishal-patil01 <vishalpatil01@hotmail.com>
2020-09-27 18:27:01 +0530

    Refactored : Dashboard UI And Added Searchbar.

--- src/components/DashBoard.vue
+++ src/components/DashBoard.vue
@@ -1,14 +1,11 @@
 <template>
-  <div class="page-container md-layout-column">
-    <md-toolbar class="md-primary">
-      <div class="md-toolbar-row">
-        <div class="md-toolbar-section-start">
-          <md-button
-            class="md-icon-button"
-            @click="showNavigation = !showNavigation"
-          >
-            <md-icon>menu</md-icon>
-          </md-button>
+  <div class="page-container">
+    <md-app>
+      <md-app-toolbar class="md-primary" md-elevation="0">
+        <md-button class="md-icon-button" @click="toggleMenu">
+          <md-icon>menu</md-icon>
+        </md-button>
+        <div>
           <span class="md-title">
             <img
               src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Google_Keep_icon.svg/1200px-Google_Keep_icon.svg.png"
@@ -16,232 +13,264 @@
             Fundoo
           </span>
         </div>
-      </div>
-    </md-toolbar>
-
-    <md-drawer
-      :md-active.sync="showNavigation"
-      md-swipeable
-      md-persistent="mini"
-    >
-      <md-list>
-        <md-list-item id="Notes" @click="selectItem('Notes')">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
+        <div class="searchDiv">
+          <i class="fa fa-search searchIcon"></i>
+          <md-autocomplete
+            class="search"
+            v-model="searchText"
+            :md-options="searchData"
+            md-layout="box"
           >
-            <path
-              d="M9 21c0 .55.45 1 1 1h4c.55 0 1-.45 1-1v-1H9v1zm3-19C8.14 2 5 5.14 5 9c0 2.38 1.19 4.47 3 5.74V17c0 .55.45 1 1 1h6c.55 0 1-.45
+            <label>Search...</label>
+          </md-autocomplete>
+        </div>
+      </md-app-toolbar>
+      <md-app-drawer
+        :md-active.sync="menuVisible"
+        md-swipeable
+        md-persistent="mini"
+        md-permanent="clipped"
+      >
+        <md-list>
+          <router-link to="#home" exact="">
+            <md-list-item id="Notes">
+              <md-icon
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M9 21c0 .55.45 1 1 1h4c.55 0 1-.45 1-1v-1H9v1zm3-19C8.14 2 5 5.14 5 9c0 2.38 1.19 4.47 3 5.74V17c0 .55.45 1 1 1h6c.55 0 1-.45
                1-1v-2.26c1.81-1.27 3-3.36 3-5.74 0-3.86-3.14-7-7-7zm2.85 11.1l-.85.6V16h-4v-2.3l-.85-.6A4.997 4.997 0 0 1 7 9c0-2.76 2.24-5 5-5s5 2.24 5 5c0 1.63-.8 3.16-2.15 4.1z"
-            ></path>
-          </svg>
-          <span class="md-list-item-text">Notes</span>
-        </md-list-item>
+                  ></path></svg
+              ></md-icon>
 
-        <md-list-item id="Reminders" @click="selectItem('Reminders')">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-          >
-            <path
-              d="M18 17v-6c0-3.07-1.63-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.64 5.36 6 7.92 6 11v6H4v2h16v-2h-2zm-2 0H8v-6c0-2.48 1.51-4.5 
+              <span class="md-list-item-text">Notes</span>
+            </md-list-item>
+          </router-link>
+
+          <router-link to="#reminders" exact="">
+            <md-list-item id="Reminders">
+              <md-icon
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M18 17v-6c0-3.07-1.63-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.64 5.36 6 7.92 6 11v6H4v2h16v-2h-2zm-2 0H8v-6c0-2.48 1.51-4.5 
               4-4.5s4 2.02 4 4.5v6zm-4 5c1.1 0 2-.9 2-2h-4c0 1.1.9 2 2 2z"
-            ></path>
-          </svg>
-          <span class="md-list-item-text">Reminders</span>
-        </md-list-item>
+                  ></path></svg
+              ></md-icon>
 
-        <md-list-item id="Edit" @click="selectItem('Edit')">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-          >
-            <path
-              d="M20.41 4.94l-1.35-1.35c-.78-.78-2.05-.78-2.83 0L13.4 6.41 3 16.82V21h4.18l10.46-10.46 2.77-2.77c.79-.78.79-2.05 0-2.83zm-14 14.12L5 19v-1.36l9.82-9.82 
+              <span class="md-list-item-text">Reminders</span>
+            </md-list-item>
+          </router-link>
+
+          <router-link to="#edit" exact="">
+            <md-list-item id="edit">
+              <md-icon>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M20.41 4.94l-1.35-1.35c-.78-.78-2.05-.78-2.83 0L13.4 6.41 3 16.82V21h4.18l10.46-10.46 2.77-2.77c.79-.78.79-2.05 0-2.83zm-14 14.12L5 19v-1.36l9.82-9.82 
               1.41 1.41-9.82 9.83z"
-            ></path>
-          </svg>
-          <span class="md-list-item-text">Edit Labels</span>
-        </md-list-item>
+                  ></path></svg
+              ></md-icon>
 
-        <md-list-item id="Archive" @click="selectItem('Archive')">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-          >
-            <path
-              d="M20.54 5.23l-1.39-1.68C18.88 3.21 18.47 3 18 
+              <span class="md-list-item-text">Edit Labels</span>
+            </md-list-item>
+          </router-link>
+
+          <router-link to="#trash" exact="">
+            <md-list-item id="trash">
+              <md-icon
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M15 4V3H9v1H4v2h1v13c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2V6h1V4h-5zm2 15H7V6h10v13z"
+                  ></path>
+                  <path d="M9 8h2v9H9zm4 0h2v9h-2z"></path></svg
+              ></md-icon>
+
+              <span class="md-list-item-text">Trash</span>
+            </md-list-item>
+          </router-link>
+
+          <router-link to="#archive" exact="">
+            <md-list-item id="archive">
+              <md-icon
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M20.54 5.23l-1.39-1.68C18.88 3.21 18.47 3 18 
           3H6c-.47 0-.88.21-1.16.55L3.46 5.23C3.17 5.57 3 6.02 3 6.5V19c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V6.5c0-.48-.17-.93-.46-1.27zM6.24 5h11.52l.83 1H5.42l.82-1zM5 
           19V8h14v11H5zm11-5.5l-4 4-4-4 1.41-1.41L11 13.67V10h2v3.67l1.59-1.59L16 13.5z"
-            ></path>
-          </svg>
-          <span class="md-list-item-text">Archive</span>
-        </md-list-item>
+                  ></path></svg
+              ></md-icon>
 
-        <md-list-item id="Trash" @click="selectItem('Trash')">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-          >
-            <path
-              d="M20.54 5.23l-1.39-1.68C18.88 3.21 18.47 3 18 
-         3H6c-.47 0-.88.21-1.16.55L3.46 5.23C3.17 5.57 3 6.02 3 6.5V19c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V6.5c0-.48-.17-.93-.46-1.27zM6.24 5h11.52l.83 
-         1H5.42l.82-1zM5 19V8h14v11H5zm11-5.5l-4 4-4-4 1.41-1.41L11 13.67V10h2v3.67l1.59-1.59L16 13.5z"
-            ></path>
-          </svg>
-          <span class="md-list-item-text">Trash</span>
-        </md-list-item>
-      </md-list>
-    </md-drawer>
-    <md-content class="main-content-area"></md-content>
+              <span class="md-list-item-text">Archive</span>
+            </md-list-item>
+          </router-link>
+        </md-list>
+      </md-app-drawer>
+
+      <md-app-content> Lorem ipsum dolor sit amet, </md-app-content>
+    </md-app>
   </div>
 </template>
 
-
 <script>
 export default {
-  name: "DashBoard",
+  name: "PersistentMini",
   data: () => ({
-    showNavigation: false,
-    showSidepanel: false,
-    prevSelection: "",
-    selected: "",
+    menuVisible: false,
+    searchText: "",
+    searchData: [],
   }),
   methods: {
-    selectItem: function (selectedId) {
-      var cssproperty = document.getElementById(selectedId).style;
-      if (this.prevSelection.length != 0) {
-        document.getElementById(this.prevSelection).removeAttribute("style", "");
-      }
-      cssproperty.backgroundColor = "#feefc3";
-      cssproperty.borderRadius = " 0 20px 20px 0px";
-
-      this.prevSelection = selectedId;
+    toggleMenu() {
+      this.menuVisible = !this.menuVisible;
     },
   },
-  created() {
-    if (localStorage.getItem("userId") == undefined) {
-      this.$router.push("/");
-    }
-  },
-  mounted(){
-     this.selectItem('Notes')
-  }
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-.page-container {
-  overflow: hidden;
-  position: relative;
-  border: 1px solid rgba(#000, 0.12);
-  background-color: white;
+.md-app {
   height: 100vh;
+  border: 1px solid rgba(#000, 0.12);
 }
-
-.main-content-area {
-  display: flex;
-  justify-content: center;
-  align-items: center;
+.md-app-container {
+  transform: none !important;
 }
-// Demo purposes only
+// drawer
 .md-drawer {
   width: 230px;
   max-width: calc(100vw - 125px);
+  vertical-align: top;
+  align-items: flex-start;
 }
-.md-active svg {
-  margin-right: 25px;
-}
-
-.md-content {
-  padding: 16px;
-}
-.app-bar {
-  background-color: rgb(237, 180, 21);
-  color: white;
-  z-index: 5;
-}
-.md-transparent {
-  background-color: rgb(237, 180, 21);
-}
-.dashboard {
-  display: flex;
-  width: 100%;
-  flex-direction: column;
-}
-.navbar {
-  display: flex;
-  flex-direction: row;
-  width: 100%;
-  align-items: center;
-}
+//app bar background
 .md-toolbar.md-theme-default.md-primary {
-  background-color: transparent;
-  box-shadow: none;
-  border-bottom: 2px solid silver;
+  background-color: white;
+  border-bottom: 1px solid rgb(223, 223, 223);
 }
-.md-toolbar .md-button ~ .md-title,
-.md-toolbar .md-icon ~ .md-title {
-  margin-left: 0px;
+//menu icon color
+.md-toolbar.md-theme-default.md-primary .md-icon {
+  color: black;
 }
-.md-icon {
-  color: black !important;
-}
-.search {
-  max-width: 650px;
-  background-color: rgb(243, 240, 240) !important;
-}
-.md-toolbar .md-autocomplete.md-theme-default.md-autocomplete-box {
-  height: 50px;
-  border-radius: 7px;
-}
-.md-drawer.md-theme-default.md-persistent-mini.md-left {
-  width: 280px;
-  height: 90vh;
-  margin-top: 1px;
-}
-.md-drawer{
-    height: 90vh;
-}
+//App Title color
 .md-title {
   color: rgb(0, 0, 0) !important;
-  margin-right: 20% !important;
-  font-size: 22px;
+  margin-right: 20%;
+  font-size: calc(0.75em + 1.2vmin);
 }
+//App icon
 img {
-  width: 46px;
-  height: 34px;
+  width: calc(0.75em + 2.9vmin);
+  height: calc(0.75em + 1.8vmin);
   padding: 0 5px;
 }
-.md-active .md-list-item:hover {
-  background-color: rgb(223,223,223);
-  border-radius: 0 20px 20px 0px !important;
+//menu icon button
+.md-icon-button {
+    margin: 0;
 }
-.md-list-item :hover {
-  background-color: rgb(223,223,223);
+//seperating two list item with margin
+li {
+  margin: 2px 0;
+}
+//change color for active link when drawer expanded
+.md-active > ul > .router-link-active {
+  background-color: #feefc3;
+  border-radius: 0 20px 20px 0px;
+}
+//change color for active link when drawer is not expanded
+.router-link-active {
+  background-color: #feefc3;
   border-radius: 30px;
 }
-.md-list-item {
-  margin: 3px 0;
+//change color for unactive link when drawer expanded
+ul > :not(.router-link-active) :hover {
+  background-color: rgb(223, 223, 223);
+  border-radius: 20px;
 }
-// .md-list-item :hover {
-//   background-color: gray;
-//   border-radius: 50px;
-// }
+//change color for unactive link when drawer is not expanded
+.md-active > ul > :not(.router-link-active) :hover {
+  background-color: rgb(223, 223, 223);
+  border-radius: 0 20px 20px 0px;
+}
+//change color list item text
 .md-list-item-text {
   font-size: 15px;
-  font-weight: bold;
+  font-weight: inherit;
   font-display: monospace;
+  color: #000;
+}
+//search
+.searchDiv {
+  margin: 10px 3vw;
+  background-color: rgb(241, 243, 244);
+  border-radius: 10px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  border-radius: 10px;
+  width: fit-content;
+}
+.search {
+  background-color: rgb(241, 243, 244) !important;
+  border-radius: 20px;
+}
+.searchIcon {
+  color: rgb(95, 99, 104);
+  margin-left: 15px;
+  font-size: 16px;
+}
+
+//search label color
+.md-autocomplete.md-theme-default.md-autocomplete-box label {
+  -webkit-text-fill-color: rgb(95, 99, 104) !important;
+}
+// search box radious and text color
+.md-toolbar .md-autocomplete.md-theme-default.md-autocomplete-box {
+  border-radius: 10px;
+  width: 30vw;
+  -webkit-text-fill-color: rgb(95, 99, 104) !important;
+}
+
+//stop clipped drawer moving down below width 600px
+@media (min-width: 0) {
+  .md-drawer.md-permanent {
+    position: relative;
+    transform: translate3D(0, 0, 0);
+  }
+}
+@media (max-width: 800px) {
+  .md-title {
+    font-size: 18px;
+    margin: 0;
+  }
+  //App icon
+  img {
+    width: 30px;
+    height: 25px;
+    padding: 0;
+  }
 }
 </style>
